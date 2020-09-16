0b1682a0688e2d6b5df8c4471d1d43cdcd7fd151
monge1h <jlo4321jl@gmail.com>
2020-09-16 13:00:57 -0600

    Add logic to controllers

--- src/mensajes/mensajes.controller.ts
+++ src/mensajes/mensajes.controller.ts
@@ -1,25 +1,51 @@
-import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
 import { CreateMensajeDto } from './dto/create-mensaje-dto';
+import { MensajesService } from './mensajes.service';
 
 @Controller('mensajes')
 export class MensajesController {
+
+    constructor(private mensajesServices: MensajesService){
+
+    }
+
     @Post()
-    create (@Body() createMensajeDto: CreateMensajeDto){
-        return 'Message created'
+    create (@Body() createMensajeDto: CreateMensajeDto, @Res() response){
+        this.mensajesServices.createMensaje(createMensajeDto).then( mensaje =>{
+            return response.status(HttpStatus.CREATED).json(mensaje);
+        }).catch(
+            ()=>{
+                response.status(HttpStatus.FORBIDDEN).json({mensaje: 'Error en la creacion del mensaje'})  
+            } 
+        )
     }
 
     @Get()
-    getAll(){
-        return "List of messages"
+    getAll(@Res() response){
+        this.mensajesServices.getAll().then(mensajes =>{
+            response.status(HttpStatus.OK).json(mensajes);
+        }).catch(()=>{
+            response.status(HttpStatus.FORBIDDEN).json({mensaje: 'Error en la obtencion del mensaje'})
+        })
     }
 
     @Put(':id')
-    update(@Body() updateMensajeDto: CreateMensajeDto) {
-        return 'Message updated'
+    update(@Body() updateMensajeDto: CreateMensajeDto, @Res() response, @Param('id') idMensaje) {
+        this.mensajesServices.updateMensaje(idMensaje, updateMensajeDto).then(mensaje=>{
+            response.status(HttpStatus.OK).json(mensaje);
+        }).catch(()=>{
+            response.status(HttpStatus.FORBIDDEN).json({mensaje: 'Error en la actualizacion del mensaje'})
+
+        })
     }
 
     @Delete(':id')
-    delete(){
-        return 'Message deleted'
+    delete(@Res() response, @Param('id') idMensaje){
+        this.mensajesServices.deleteMensaje(idMensaje).then(res=>{
+            response.status(HttpStatus.OK).json(res)
+        }
+        ).catch(()=>{
+            response.status(HttpStatus.FORBIDDEN).json({mensaje: 'Error en la eliminacion del mensaje'})
+        })
     }
 }
