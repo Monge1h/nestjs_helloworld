ac9780757ddf6012baec8c2701059032f9f032fa
monge1h <jlo4321jl@gmail.com>
2020-09-15 20:54:37 -0600

    Add http methods to controllers

--- src/mensajes/mensajes.controller.ts
+++ src/mensajes/mensajes.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { CreateMensajeDto } from './dto/create-mensaje-dto';
 
 @Controller('mensajes')
-export class MensajesController {}
+export class MensajesController {
+    @Post()
+    create (@Body() createMensajeDto: CreateMensajeDto){
+        return 'Message created'
+    }
+
+    @Get()
+    getAll(){
+        return "List of messages"
+    }
+
+    @Put(':id')
+    update(@Body() updateMensajeDto: CreateMensajeDto) {
+        return 'Message updated'
+    }
+
+    @Delete(':id')
+    delete(){
+        return 'Message deleted'
+    }
+}
