55e65f21d3b345f2be322476fb30234d9f5c96dd
monge1h <jlo4321jl@gmail.com>
2020-09-15 20:55:00 -0600

    Add typeorm to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, MensajesController],
   providers: [AppService],
 })
