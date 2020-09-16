828684adad4e3db75bcf3ce63e46454778b6b149
monge1h <jlo4321jl@gmail.com>
2020-09-15 22:04:25 -0600

    Create service layer for mensaje

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MensajesController } from './mensajes/mensajes.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MensajesService } from './mensajes/mensajes.service';
+import { Mensaje } from './mensajes/entities/mensaje.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature(Mensaje)],
   controllers: [AppController, MensajesController],
-  providers: [AppService],
+  providers: [AppService, MensajesService],
 })
 export class AppModule {}
